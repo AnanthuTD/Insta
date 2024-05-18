a353a16fa81918b600f0bc942391f6117e55b53e
AnanthuTD <ananthutd2021@gmail.com>
2024-05-18 19:18:27 +0530

    added users

---
+++ components/story/script.js
@@ -0,0 +1,164 @@
+function setScrollRight(bool) {
+	if (bool) {
+		document.getElementById("scroll-right").style.display = "flex";
+	} else {
+		document.getElementById("scroll-right").style.display = "none";
+	}
+}
+
+function setScrollLeft(bool) {
+	if (bool) {
+		document.getElementById("scroll-left").style.display = "flex";
+	} else {
+		document.getElementById("scroll-left").style.display = "none";
+	}
+}
+
+setScrollRight(false);
+
+setScrollLeft(false);
+
+function handleClickLeft() {
+	const container = document.getElementById("scroll-container");
+	const containerWidth = container?.getBoundingClientRect().width || 0;
+	const contentWidth = container?.scrollWidth || 0;
+	const scrollAmount = containerWidth / 2;
+
+	if (container && container.scrollLeft !== 0) {
+		container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+
+		setScrollRight(true);
+
+		if (Math.round(container.scrollLeft + containerWidth) >= contentWidth) {
+			setScrollLeft(false);
+		}
+	}
+}
+
+function handleClickRight() {
+	const container = document.getElementById("scroll-container");
+	const containerWidth = container?.getBoundingClientRect().width || 0;
+	const contentWidth = container?.scrollWidth || 0;
+	const scrollAmount = containerWidth / 2;
+
+	if (container && contentWidth - container.scrollLeft > containerWidth) {
+		container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+
+		setScrollLeft(true);
+
+		if (Math.round(contentWidth - container.scrollLeft) === contentWidth) {
+			setScrollRight(false);
+		}
+	}
+}
+
+function check() {
+	const container = document.getElementById("scroll-container");
+	const containerWidth = container?.getBoundingClientRect().width || 0;
+	const contentWidth = container?.scrollWidth || 0;
+
+	if (container && contentWidth > containerWidth) {
+		setScrollRight(true);
+	}
+}
+
+const STORIES = [
+	{
+		username: "leomessi",
+		avatar: "public/leomessi/avatar.jpg",
+		name: "Leo Messi",
+		image: "public/leomessi/1.jpg",
+	},
+	{
+		username: "cristiano",
+		avatar: "public/cristiano/avatar.jpg",
+		name: "Cristiano Ronaldo",
+		image: "public/cristiano/1.jpg",
+	},
+	{
+		username: "zuck",
+		avatar: "public/zuck/avatar.jpg",
+		name: "Mark Zuckerberg",
+		image: "public/zuck/1.jpg",
+	},
+	{
+		username: "_ananthu_td_",
+		avatar: "public/_ananthu_td_/avatar.jpg",
+		name: "Ananthu T D",
+		image: "public/_ananthu_td_/1.jpg",
+	},
+	{
+		username: "emmawatson",
+		avatar: "public/emmawatson/avatar.jpg",
+		name: "Emma Watson",
+		image: "public/emmawatson/1.jpg",
+	},
+	{
+		username: "therock",
+		avatar: "public/therock/avatar.jpg",
+		name: "Dwayne Johnson",
+		image: "public/therock/1.jpg",
+	},
+
+
+
+	{
+		username: "cristiano",
+		avatar: "public/cristiano/avatar.jpg",
+		name: "Cristiano Ronaldo",
+		image: "public/cristiano/1.jpg",
+	},
+	{
+		username: "zuck",
+		avatar: "public/zuck/avatar.jpg",
+		name: "Mark Zuckerberg",
+		image: "public/zuck/1.jpg",
+	},
+	{
+		username: "_ananthu_td_",
+		avatar: "public/_ananthu_td_/avatar.jpg",
+		name: "Ananthu T D",
+		image: "public/_ananthu_td_/1.jpg",
+	},
+	{
+		username: "leomessi",
+		avatar: "public/leomessi/avatar.jpg",
+		name: "Leo Messi",
+		image: "public/leomessi/1.jpg",
+	},
+	{
+		username: "therock",
+		avatar: "public/therock/avatar.jpg",
+		name: "Dwayne Johnson",
+		image: "public/therock/1.jpg",
+	},
+	{
+		username: "emmawatson",
+		avatar: "public/emmawatson/avatar.jpg",
+		name: "Emma Watson",
+		image: "public/emmawatson/1.jpg",
+	},
+];
+
+async function loadStories() {
+	const id = "story-ring",
+		componentPath = "./components/story";
+
+	const component = await fetch(componentPath).then((data) => data.text());
+	if (!component) return;
+	const node = document.getElementById(id);
+	for (let i = 0; i < STORIES.length; i++) {
+		let temp = new DOMParser().parseFromString(component, "text/html");
+		temp
+			.getElementById("story-avatar")
+			.setAttribute("href", STORIES[i].avatar);
+		node.innerHTML += new XMLSerializer().serializeToString(temp);
+	}
+
+	// 1 millisecond delay to wait for the scroll-container to be visible
+	setTimeout(()=>{
+		check();
+	}, 1)
+}
+
+loadStories();
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 	loadComponents("menu", "./components/menu");
-	loadComponents("story-ring", "./components/story", "", 10);
-	loadPost("post", "./components/post", "./components/post/script.js", 10);
+	loadScript("./components/story/script.js");
+	loadPost("post", "./components/post", "./components/post/script.js");
+	loadSuggestedAccounts()
 });
 
 async function loadComponents(id, componentPath, scriptUrl, num = 1) {
@@ -20,12 +21,56 @@ function loadScript(url) {
 	document.body.appendChild(script);
 }
 
-async function loadPost(id, componentPath, scriptUrl, num = 1) {
+const ACCOUNTS = [
+	{
+		username: "leomessi",
+		avatar: "public/leomessi/avatar.jpg",
+		name: "Leo Messi",
+		image: "public/leomessi/1.jpg",
+	},
+	{
+		username: "cristiano",
+		avatar: "public/cristiano/avatar.jpg",
+		name: "Cristiano Ronaldo",
+		image: "public/cristiano/1.jpg",
+	},
+	{
+		username: "zuck",
+		avatar: "public/zuck/avatar.jpg",
+		name: "Mark Zuckerberg",
+		image: "public/zuck/1.jpg",
+	},
+	{
+		username: "_ananthu_td_",
+		avatar: "public/_ananthu_td_/avatar.jpg",
+		name: "Ananthu T D",
+		image: "public/_ananthu_td_/1.jpg",
+	},
+	{
+		username: "emmawatson",
+		avatar: "public/emmawatson/avatar.jpg",
+		name: "Emma Watson",
+		image: "public/emmawatson/1.jpg",
+	},
+	{
+		username: "therock",
+		avatar: "public/therock/avatar.jpg",
+		name: "Dwayne Johnson",
+		image: "public/therock/1.jpg",
+	},
+];
+
+async function loadPost(id, componentPath, scriptUrl) {
 	const component = await fetch(componentPath).then((data) => data.text());
 	if (!component) return;
 	const node = document.getElementById(id);
-	for (let i = 0; i < num; i++) {
+	for (let i = 0; i < ACCOUNTS.length; i++) {
 		let temp = new DOMParser().parseFromString(component, "text/html");
+		temp.getElementById("avatar").setAttribute("href", ACCOUNTS[i].avatar);
+		temp.getElementById("username").innerHTML = ACCOUNTS[i].username;
+		temp.getElementById("name").innerHTML = ACCOUNTS[i].name;
+		temp.getElementById("name").innerHTML = ACCOUNTS[i].name;
+		temp.getElementById("post-img").setAttribute("src", ACCOUNTS[i].image);
 		temp.getElementById("follow").id = "follow-btn-" + i;
 		temp.getElementById("like").id = "like-btn-" + i;
 		temp.getElementById("save").id = "save-btn-" + i;
@@ -34,4 +79,22 @@ async function loadPost(id, componentPath, scriptUrl, num = 1) {
 		node.innerHTML += new XMLSerializer().serializeToString(temp);
 	}
 	if (scriptUrl) loadScript(scriptUrl);
+}
+
+async function loadSuggestedAccounts() {
+	const node = document.getElementById('suggested-accounts');
+	for (let i = 0; i < ACCOUNTS.length; i++) {
+		let account = `<div  class="flex my-3 cursor-pointer"><img
+		src="${ACCOUNTS[i].avatar}"
+		width="44px"
+		height="44px"
+		alt=""
+		class="rounded-full"
+	/>
+	<p class="flex items-center mx-4 text-sm">${ACCOUNTS[i].username}</p>
+	<button class="ml-auto text-xs font-medium text-instaBlue">
+		Follow
+	</button></div>`;
+		node.innerHTML += account;
+	}
 }
